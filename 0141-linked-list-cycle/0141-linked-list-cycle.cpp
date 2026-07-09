8cc45250e6960a6c5ffab068c3cd8a7abc1806a8
Priya Sharma <priyashr1904@gmail.com>
2026-07-09 13:00:54 +0530

    Time: 8 ms (83.24%), Space: 11.8 MB (81.49%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        // Step 2: Traverse the linked list with
+        // the slow and fast pointers
+        while (fast != nullptr && fast->next != nullptr) {
+            // Move slow one step
+            slow = slow->next;
+            // Move fast two steps
+            fast = fast->next->next;
+
+            // Check if slow and fast pointers meet
+            if (slow == fast) {
+                return true;  // Loop detected
+            }
+        }
+
+        // If fast reaches the end of the list,
+        // there is no loop
+        return false; 
+    }
+};
