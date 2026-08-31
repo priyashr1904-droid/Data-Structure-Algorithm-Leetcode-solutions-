aa6d46e9b5d7a9f8521440ce06977d8ad7cd8bfe
Priya Sharma <priyashr1904@gmail.com>
2026-08-31 22:14:48 +0530

    Time: 4 ms (99.87%), Space: 106.1 MB (55.32%) - LeetHub

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return new int[]{-1, -1};
+        }
+
+        ListNode prev = head;
+        ListNode curr = head.next;
+        int index = 1;
+
+        int firstCriticalIndex = -1;
+        int prevCriticalIndex = -1;
+        int minDistance = Integer.MAX_VALUE;
+
+        while (curr.next != null) {
+            if ((curr.val > prev.val && curr.val > curr.next.val) || 
+                (curr.val < prev.val && curr.val < curr.next.val)) {
+                
+                if (firstCriticalIndex == -1) {
+                    firstCriticalIndex = index;
+                } else {
+                    minDistance = Math.min(minDistance, index - prevCriticalIndex);
+                }
+                prevCriticalIndex = index;
+            }
+
+            prev = curr;
+            curr = curr.next;
+            index++;
+        }
+
+        if (firstCriticalIndex == -1 || firstCriticalIndex == prevCriticalIndex) {
+            return new int[]{-1, -1};
+        }
+
+        int maxDistance = prevCriticalIndex - firstCriticalIndex;
+        return new int[]{minDistance, maxDistance};
+    }
+}
